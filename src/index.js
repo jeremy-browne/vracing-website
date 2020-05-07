2b3e3c7b9f8e63ddccc991b4e946d11c1efcd992
= <=>
2020-05-07 21:21:42 +1000

    cleaned up pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.facebookLabel = "VRacing Facebook";
-        this.twitterLabel = "Vracing Twitter";
+        this.twitterLabel = "VRacing Twitter";
         this.instagramLabel = "VRacing Instagram";
         this.linkedinLabel = "VRacing LinkedIn";
         this.emailLabel = "VRacing e-mail"
@@ -14,18 +14,21 @@ class App extends React.Component {
     render() {
         return (
             <div className="container">
-                <div className="content">
-                    <img src={require("./images/2.png")} alt="Vracing Logo" />
+                <div className="content fade-in">
+                    <img src={require("./images/2.png")} alt="Vracing Logo"/>
                     <p>We're taking a bit of a break during COVID-19, but rest assured we'll be back, and with better racing than ever.</p>
 
                     <div className="footer">
-                        <a href="https://www.facebook.com/vracing.global/" className="fa fa-facebook" alt={this.facebookLabel}><span>{this.facebookLabel}</span></a>
-                        <a href="https://www.instagram.com/vracing.global/" className="fa fa-twitter" alt={this.twitterLabel}><span>{this.twitterLabel}</span></a>
-                        <a href="https://twitter.com/VRacing_global" className="fa fa-instagram" alt={this.instagramLabel}><span>{this.instagramLabel}</span></a>
-                        <a href="https://www.linkedin.com/company/vracingglobal" className="fa fa-linkedin" alt={this.instagramLabel}><span>{this.linkedinLabel}</span></a>
-                        <a href="mailto:contact@vracingsim.com" className="fa fa-envelope"alt={this.emailLabel}><span>{this.emailLabel}</span></a>
+                        <a href="https://www.facebook.com/vracing.global/" className="fab fa-facebook" alt={this.facebookLabel}><span>{this.facebookLabel}</span></a>
+                        <a href="https://www.instagram.com/vracing.global/" className="fab fa-instagram" alt={this.instagramLabel}><span>{this.instagramLabel}</span></a>
+                        <a href="https://twitter.com/VRacing_global" className="fab fa-twitter" alt={this.twitterLabel}><span>{this.twitterLabel}</span></a>
+                        <a href="https://www.linkedin.com/company/vracingglobal" className="fab fa-linkedin" alt={this.instagramLabel}><span>{this.linkedinLabel}</span></a>
+                        <a href="mailto:contact@vracingsim.com" className="fas fa-envelope"alt={this.emailLabel}><span>{this.emailLabel}</span></a>
+                        <div className="lowerText">
+                            <p>&copy; VRacing 2020</p>
+                        </div>
+                        
                     </div>
-                    <p className="lowerText">&copy; VRacing 2020</p>
                 </div>
             </div>
         )
